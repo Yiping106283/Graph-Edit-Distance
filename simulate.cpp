c6a22235297422aa7e4b8fba6922eab0d05f60e7
Helen Ma <32121800+GoAhead106283@users.noreply.github.com>
2018-08-21 11:13:01 +0800

    add

--- simulate.cpp
+++ simulate.cpp
@@ -18,18 +18,14 @@ void random_X(int n, int m) {
 
     set<int>::iterator iter;
 
-    //cout << m << " " << n << endl;
     double prob = max(0.0, 1 - (double(m) / double(n)));
-    //cout << "prob = " << prob << endl;
     for (iter = pool.begin(); iter != pool.end(); ++iter) {
-        //cout << "*iter = " << *iter << endl;
         double rand_p = (double)(rand() % 100) / (double)101;
         if (rand_p < prob) {
             X[*iter] = -1;
         } else if (X[*iter] == -1) { 
             int Y[100];
             memset(Y, -1, sizeof(Y));
-
             int flag = 0, j = 0;
             for (int scan = 0; scan < m; scan++) { 
                 flag = 0;
@@ -45,7 +41,6 @@ void random_X(int n, int m) {
                 }
             }
 
-            // 计算t
             int t = 1;
             for (int i = 0; i < 100; i++) {
                 if (Y[i] == -2) {
@@ -55,7 +50,6 @@ void random_X(int n, int m) {
             }
 
             double rand_t = (double)(rand() % 100) / (double)101;
-            //cout << "rand_t = " << rand_t << endl;
             if (rand_t > prob) {
                 //cout << "t = " << t << endl;
                 X[*iter] = Y[rand() % t];
@@ -63,7 +57,6 @@ void random_X(int n, int m) {
 
         }
     }
-
 
     set<int> permpool;
     for (int i = 0; i < C; i++) {
@@ -77,10 +70,7 @@ void random_X(int n, int m) {
     for (it = permpool.begin(); it != permpool.end(); ++it) {
         perm[i++] = X[*it];
     }
-    
-
    
-
     random_shuffle(perm, perm + permpool.size());
     i = 0;
     for (it = permpool.begin(); it != permpool.end(); ++it) {
